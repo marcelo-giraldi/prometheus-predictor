e88fa36ab31ef421b190d02623d97812288f7d1b
Marilze <marilzerocha@gmail.com>
2021-05-18 13:35:36 -0300

    Configuração do prom_client

--- src/prom_client.py
+++ src/prom_client.py
@@ -1,4 +1,14 @@
-pc = None
+from config_parser import config
+from prometheus_api_client import PrometheusConnect
+from prometheus_api_client.utils import parse_timedelta
+from datetime import datetime
 
-def query:
-    pc.getrange...
+pc = PrometheusConnect(
+    url=config.prometheus.url,
+    headers={"Autorization": "bearer "+ config.prometheus.access_token},
+    disable_ssl=True,
+)
+
+def query(expr, time_range, resolution)
+    delta = parse_timedelta("now", time_range)
+    return custom_query_range(query: expr, start_time: datetime.now() - delta, end_time: datetime.now(), step: resolution)
