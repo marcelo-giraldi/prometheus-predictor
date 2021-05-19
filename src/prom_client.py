198d4f70950e8302ec80ccade10542f6401c2925
Marilze <marilzerocha@gmail.com>
2021-05-19 09:43:36 -0300

    Correção e teste do código do prom_client

--- src/prom_client.py
+++ src/prom_client.py
@@ -3,12 +3,16 @@ from prometheus_api_client import PrometheusConnect
 from prometheus_api_client.utils import parse_timedelta
 from datetime import datetime
 
+headers = None
+if config["settings"]["prometheus"]["access_token"]:
+    headers = {"Autorization": "bearer "+ config["settings"]["prometheus"]["access_token"]}
+
 pc = PrometheusConnect(
-    url=config.prometheus.url,
-    headers={"Autorization": "bearer "+ config.prometheus.access_token},
+    url=config["settings"]["prometheus_url"],
+    headers=headers,
     disable_ssl=True,
 )
 
-def query(expr, time_range, resolution)
+def query(expr, time_range, resolution):
     delta = parse_timedelta("now", time_range)
-    return custom_query_range(query: expr, start_time: datetime.now() - delta, end_time: datetime.now(), step: resolution)
+    return pc.custom_query_range(query = expr, start_time = datetime.now() - delta, end_time = datetime.now(), step = resolution)
